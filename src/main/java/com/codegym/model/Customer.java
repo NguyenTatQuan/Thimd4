ff8616d73817a7628b75b4094ea4ec6ff2db4c5e
kikon123 <tatquan2001@gmail.com>
2024-06-12 09:54:27 +0700

    up

---
+++ src/main/java/com/codegym/model/Customer.java
@@ -0,0 +1,20 @@
+package com.codegym.model;
+
+
+import lombok.Data;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customer")
+@Data
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String Name;
+    private String typeofservice;
+    private String daytrading;
+    private String price;
+    private String acreage;
+}
